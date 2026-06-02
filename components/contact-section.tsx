9762a4ced676d6719f4240df49efacfb7411b279
Elham Rahimi <elhamrahimi208@gmail.com>
2026-06-02 13:20:19 +0200

    schedule fixed

--- components/contact-section.tsx
+++ components/contact-section.tsx
@@ -5,9 +5,10 @@ import { MapPin, Phone, Clock, Mail } from 'lucide-react'
 
 export function ContactSection() {
   const openingHours = [
-    { day: 'Montag - Freitag', hours: '11:00 - 15:00, 17:00 - 22:00' },
-    { day: 'Samstag', hours: '12:00 - 22:00' },
-    { day: 'Sonntag', hours: '12:00 - 21:00' },
+    { day: 'Montag - Donnerstag', hours: '11:30 - 15:00, 17:00 - 22:00' },
+    {day: 'Freitag', hours: '11:00 - 15:00, 17:00 - 23:00' },
+    { day: 'Samstag', hours: '11:30 - 15:00, 17:00 - 23:00' },
+    { day: 'Sonntag', hours: '15:00 - 22:00' },
   ]
 
   return (
